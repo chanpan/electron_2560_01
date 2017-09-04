f6d9de167c4f0cf58e299b222d82b08e89668a6c
chanpan <chanpan.nuttaphon@gmail.com>
2017-09-04 19:22:43 +0700

    about page

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const electron = require("electron");
-const {app, BrowserWindow,ipcMain} = electron;
+const {app, BrowserWindow} = electron;
 
 app.on("ready", ()=>{
-	let win = new BrowserWindow({width:800,height:600});
+	let win = new BrowserWindow({
+        width:800,
+        height:600
+    });
 	win.loadURL(`file://${__dirname}/index.html`);
 	win.on('closed', function () { win = null; });
 });
+
+exports.openWindow = (filename) =>{
+	let win = new BrowserWindow({
+        width:600,
+        height:400
+    });
+	win.loadURL(`file://${__dirname}/${filename}.html`);
+}
