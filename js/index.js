ea358876b61f42811b045066379f5bad40965c4b
chanpan <chanpan.nuttaphon@gmail.com>
2017-09-04 19:50:17 +0700

    Jquery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 const remote = require("electron").remote;
 const main = remote.require('./main.js');
-
-var button = document.createElement('button');
-button.textContent = "About";
-button.addEventListener('click',()=>{
+ 
+$("#btnAbout").click(()=>{
     var window = remote.getCurrentWindow();
     main.openWindow('about');
-    //window.close();
-},false);
-document.body.appendChild(button);
+});
+ 
